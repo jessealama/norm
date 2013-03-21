cfac453c0b4e49d4ce30d3780b5ea016df84fa91
Jesse Alama <jesse.alama@gmail.com>
2013-03-21 14:07:53 +0100

    Ensure only one.

--- fol.lisp
+++ fol.lisp
@@ -18,6 +18,27 @@
     :initarg :arguments
     :reader arguments)))
 
+(defvar *formula-table* (make-hash-table :test #'eql))
+
+(defmethod make-instance :before ((class (eql 'atomic-formula))
+				  &rest initargs &key &allow-other-keys)
+  (error "haha!"))
+
+(defmethod make-instance :around ((class (eql 'atomic-formula))
+				  &rest initargs &key &allow-other-keys)
+  (let ((predicate-tail (member :predicate initargs)))
+    (if predicate-tail
+	(if (rest predicate-tail)
+	    (let ((predicate (first predicate-tail)))
+	      (multiple-value-bind (already-created present-p)
+		  (gethash predicate *formula-table*)
+		(if present-p
+		    already-created
+		    (let ((instance (call-next-method)))
+		      (setf (gethash predicate *formula-table*) instance)))))
+	    (error "Invalid call to MAKE-INSTANCE for ATOMIC-FORMULA (:predicate keyword is present, but this keyword ends the list of initialization arguments)."))
+	(error "Invalid call to MAKE-INSTANCE for ATOMIC-FORMULA (mandatory :predicate keyword is missing)."))))
+
 (defclass negation (formula)
   ((argument
     :type formula
